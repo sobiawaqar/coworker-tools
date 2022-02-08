9ecf329f4fd72323bdfe828aa086e0bd8b58dfe4
Sobia Waqar <sobiawaqar27'gmail.com>
2022-02-08 10:16:30 +0000

    data is stored in object

--- members.js
+++ members.js
@@ -1,3 +1,50 @@
-const members = [];
+const members = [
+  {
+    "first name": "John",
+    "last name": "Doe",
+    "job title": "Software Engineer ",
+    department: "engineering",
+    manager: null,
+    location: "Glasgow",
+    firstAider: "No",
+    userSlack: "johndoe",
+    timezone: "America/Los_Angeles",
+  },
+  {
+    "first name": "Jane",
+    "last name": "Doe",
+    "job title": "Software Engineer ",
+    department: "engineering",
+    manager: "John Doe",
+    location: "Leeds",
+    firstAider: "No",
+    userSlack: "janedoe",
+    timezone: "America/Los_Angeles",
+  },
+  {
+    "first name": "Bob",
+    "last name": "Doe",
+    "job title": "Software Engineer ",
+    department: "engineering",
+    manager: "Jane Doe",
+    location: "Cape Town",
+    firstAider: "No",
+    userSlack: "bobdoe",
+    timezone: "America/Los_Angeles",
+  },
+  {
+    "first name": "Sally",
+    "last name": "Doe",
+    "job title": "Software Engineer ",
+    department: "engineering",
+    manager: "Jane Doe",
+    location: "Rome",
+    firstAider: "No",
+    userSlack: "sallydoe",
+    timezone: "America/Los_Angeles",
+  },
+];
 
 module.exports = members;
+
+
